7c8233d4bf6c072b96e1cc76cec5512fc16be9b6
nurdinah <dinah.saja.bjb@gmail.com>
2020-04-25 04:13:06 +0700

    mataKuliah

---
+++ src/Loops/ArrayMatakuliah.java
@@ -0,0 +1,14 @@
+package Loops;
+
+public class ArrayMatakuliah {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangkat Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+    }
+}
