50c476f4dc5b511110ba641c699b08a8b58a2dd6
nurdinah <dinah.saja.bjb@gmail.com>
2020-04-25 03:38:22 +0700

    whiledo

--- src/Loops/DoWhile.java
+++ src/Loops/DoWhile.java
@@ -1,20 +1,19 @@
 package Loops;
 
-public class While {
-    public static void main(String[] args) {
+public class DoWhile {
+    public static void main(String[] args){
         int isiGelas = 0;
         int penuh = 225;
 
         System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
         System.out.println("Isi Gelas Ketika Penuh : " + penuh + "m1");
 
-        while(isiGelas != penuh){
+        do {
             isiGelas++;
             System.out.println("Sedang mengisi gelas..");
             System.out.println("Isi Gelas Sekarang : " + isiGelas + "m1");
-        }
+        }while(isiGelas != penuh);
 
         System.out.println("Finale: Isi Gelas Sekarang : " + isiGelas + "m1");
-
     }
 }
